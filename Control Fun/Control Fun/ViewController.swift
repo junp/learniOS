277a3590ba27dd7c0942ecf6e2e76ecf6fa4e973
junp <junpmail@163.com>
2016-02-24 23:58:59 +0800

    Control Fun

---
+++ Control Fun/Control Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Control Fun
+//
+//  Created by 郑 湛波 on 16/2/24.
+//  Copyright (c) 2016年 Apress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
